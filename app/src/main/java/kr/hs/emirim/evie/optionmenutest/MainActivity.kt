7a9ac38ff54169ac1a7184484de5edf9a6862487
ezi-s-u <ljsgoo0617@gmail.com>
2023-08-24 19:16:43 +0900

    ex) 7-1 optionMenu 완성

--- app/src/main/java/kr/hs/emirim/evie/optionmenutest/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/optionmenutest/MainActivity.kt
@@ -1,8 +1,10 @@
 package kr.hs.emirim.evie.optionmenutest
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.LinearLayout
 
@@ -17,10 +19,43 @@ class MainActivity : AppCompatActivity() {
         btn = findViewById(R.id.btn)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean { // 메뉴
         super.onCreateOptionsMenu(menu)
         var mInflater = menuInflater
         mInflater.inflate(R.menu.menu1, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // 이벤트
+        when(item.itemId){
+            R.id.itemRed -> {
+                linear.setBackgroundColor(Color.RED)
+                return true
+            }
+            R.id.itemBlue -> {
+                linear.setBackgroundColor(Color.BLUE)
+                return true
+            }
+            R.id.itemMagenta -> {
+            linear.setBackgroundColor(Color.MAGENTA)
+            return true
+            }
+
+            R.id.subRotate -> { // 항목 중에 sub
+                btn.rotation += 45f
+                return true
+            }
+
+            R.id.subSize -> { // 항목 중에 sub
+                btn.scaleX *= 2f
+                return true
+            }
+
+            R.id.subInit -> {
+                btn.rotation = 0f
+                return true
+            }
+        }
+        return false
     }
 }
